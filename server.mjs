49ee493acac8e36e27b60b05edb46d164434a5b1
Surghe <Milesiii.Irving@gmail.com>
2025-04-15 00:49:24 -0600

    second commit

--- server.mjs
+++ server.mjs
@@ -1,11 +1,25 @@
 import express from "express";
+import fs from "fs";
+import bikeRoutes from "./routes/bikeRoutes.mjs"
 
 const app = express();
 const PORT = 3000 || 3001;
 
 
+app.engine('template', (filePath, options, callback)=>{
+    fs.readFile(filePath, (err, content)=>{
+        if(err) return callback(err);
 
+        const rendered = content.toString()
 
+        return callback(null, rendered);
+    });
+});
+
+app.set('views', './views'); 
+app.set('view engine', 'template');
+
+app.use('/bike', routes);
 
 
 app.use((err, req, res, next) => {
